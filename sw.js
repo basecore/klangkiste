1cd3b1f8500de8d74eaa39300930d2d73e38a3d7
basecore <roesser.sebastian@gmail.com>
2025-12-20 13:38:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './assets/limit.mp3',
   './assets/img/hintergrund.jpg',
   './assets/icons/icon.png',
   './assets/icons/icon512_maskable.png',
